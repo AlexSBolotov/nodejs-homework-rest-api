e0ee61984c5c8acf47bd5c0d952a210510930bd1
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-06-28 20:20:42 +0300

    before_check
    completed with additional tasks

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -2,8 +2,22 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 const { Contact } = require("../models/contact.model");
 
 const geAllContacts = async (req, res) => {
-  const result = await Contact.find();
-  res.status(200).json(result);
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+  if (favorite === undefined) {
+    const result = await Contact.find({ owner }, "", {
+      skip,
+      limit,
+    }).populate("owner", "email");
+    res.status(200).json(result);
+  } else {
+    const result = await Contact.find({ owner, favorite }, "", {
+      skip,
+      limit,
+    }).populate("owner", "email");
+    res.status(200).json(result);
+  }
 };
 const getOneContact = async (req, res) => {
   const { id } = req.params;
@@ -14,7 +28,8 @@ const getOneContact = async (req, res) => {
   res.status(200).json(result);
 };
 const postContact = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
   res.status(201).json(result);
 };
 const updateContact = async (req, res) => {
